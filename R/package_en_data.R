ca1633fd69fa044b20575ba9ed8b9de9fb92e508
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-12-16 13:27:14 +0100

    page.height documentation was missing a description

--- R/package_en_data.R
+++ R/package_en_data.R
@@ -1,5 +1,5 @@
 #' @title Package with several ggplot2 themese
-#' @description Lod this package AFTER loading ggplot2. theme_INBO() will be set as the default theme. 
+#' @description Lod this package AFTER loading ggplot2. theme_INBO() will be set as the default theme.
 #' @name INBOtheme-package
 #' @aliases INBOtheme
 #' @docType package
@@ -9,7 +9,7 @@
 NULL
 
 #'@name inbo.hoofd
-#'@aliases inbo.steun.donkerroos inbo.steun.blauw inbo.steun.geelgroen inbo.oranje inbo.rood inbo.bruinrood inbo.grijs inbo.lichtgrijs inbo.grijsblauw inbo.lichtblauw inbo.achtergrond inbo.munt inbo.lichtgroen inbo.donkergroen inbo.groen inbo.geelgr 
+#'@aliases inbo.steun.donkerroos inbo.steun.blauw inbo.steun.geelgroen inbo.oranje inbo.rood inbo.bruinrood inbo.grijs inbo.lichtgrijs inbo.grijsblauw inbo.lichtblauw inbo.achtergrond inbo.munt inbo.lichtgroen inbo.donkergroen inbo.groen inbo.geelgr
 #'@title Colour according to the INBO style guide version >= 2015
 #'@author Thierry Onkelinx, Oona Op de Weerdt, Nicole De Groof
 #'@description Colour according to the INBO style guide as hexadecimal values.
@@ -64,6 +64,7 @@ NULL
 #'@name page.height
 #'@aliases page.width column.width medium.width
 #'@title Standard dimensions for the Elsevier style guide
+#'@description Several standard dimensions for the Elsevier style guide
 #'\itemize{
 #'  \item{page.height}{Maximal height of a figuur (in inch)}
 #'  \item{page.width}{Width of a figure covering two columns (in inch)}
