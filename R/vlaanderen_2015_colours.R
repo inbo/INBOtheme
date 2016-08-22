ddd5351bad1c7526a966d3c4e1ce684de7dbfced
ThierryO <Thierry.Onkelinx@inbo.be>
2016-08-22 17:19:05 +0200

    add vlaanderen 2015 palette

---
+++ R/vlaanderen_2015_colours.R
@@ -0,0 +1,21 @@
+#' A colour pallet according the Vlaanderen style guide version >= 2015.
+#'
+#' @export
+#' @param n The number of colours
+#' @author Thierry Onkelinx
+#' @return a vector of n hexadecimal values defining the colours.
+#' @references http://webstijlgids.vlaanderen.be/element/kleurgebruik
+vlaanderen.2015.colours <- function(n) {
+  palette <- c(
+    vl.black, vl.grey4, vl.lightgreen, vl.lightbrown, vl.lightblue, vl.lightred,
+    vl.darkgreen, vl.darkbrown, vl.darkblue, vl.darkred
+  )
+  if (n > length(palette)) {
+    warning(
+      "generated palette has duplicated colours. The palette has only ",
+      length(palette),
+      " unique colours."
+    )
+  }
+  rep(palette, length.out = n)
+}
