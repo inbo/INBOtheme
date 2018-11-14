19d0f8aae9d7ad45aa6b663a67579056b5bb95d9
ThierryO <thierry.onkelinx@inbo.be>
2018-11-14 11:55:48 +0100

    use markdown in colour documentation

--- R/package_en_data.R
+++ R/package_en_data.R
@@ -1,5 +1,5 @@
 #' @title Package with several ggplot2 themese
-#' @description Lod this package AFTER loading ggplot2. theme_INBO() will be set as the default theme.
+#' @description Load this package AFTER loading ggplot2. theme_INBO() will be set as the default theme.
 #' @name INBOtheme-package
 #' @aliases INBOtheme
 #' @docType package
@@ -8,104 +8,100 @@
 #' @seealso \code{\link{theme_INBO}}, \code{\link{theme_elsevier}}
 NULL
 
-#'@name inbo.hoofd
-#'@aliases inbo.steun.donkerroos inbo.steun.blauw inbo.steun.geelgroen inbo.oranje inbo.rood inbo.bruinrood inbo.grijs inbo.lichtgrijs inbo.grijsblauw inbo.lichtblauw inbo.achtergrond inbo.munt inbo.lichtgroen inbo.donkergroen inbo.groen inbo.geelgr
+#'@name inbo_colours_2015
+#'@rdname inbo_colours_2015
+#'@aliases inbo.hoofd inbo.steun.donkerroos inbo.steun.blauw inbo.steun.geelgroen inbo.oranje inbo.rood inbo.bruinrood inbo.grijs inbo.lichtgrijs inbo.grijsblauw inbo.lichtblauw inbo.achtergrond inbo.munt inbo.lichtgroen inbo.donkergroen inbo.groen inbo.geelgr
 #'@title Colour according to the INBO style guide version >= 2015
 #'@author Thierry Onkelinx, Oona Op de Weerdt, Nicole De Groof
+#'@family colours
 #'@description Colour according to the INBO style guide as hexadecimal values.
-#'\itemize{
-#'  \item inbo.hoofd
-#'  \item inbo.steun.donkerroos
-#'  \item inbo.steun.blauw
-#'  \item inbo.steun.geelgroen
-#'  \item inbo.oranje
-#'  \item inbo.rood
-#'  \item inbo.bruinrood
-#'  \item inbo.grijs
-#'  \item inbo.lichtgrijs
-#'  \item inbo.grijsblauw
-#'  \item inbo.lichtblauw
-#'  \item inbo.achtergrond
-#'  \item inbo.munt
-#'  \item inbo.lichtgroen
-#'  \item inbo.donkergroen
-#'  \item inbo.groen
-#'  \item inbo.geelgr
-#'}
+#'- inbo.hoofd
+#'- inbo.steun.donkerroos
+#'- inbo.steun.blauw
+#'- inbo.steun.geelgroen
+#'- inbo.oranje
+#'- inbo.rood
+#'- inbo.bruinrood
+#'- inbo.grijs
+#'- inbo.lichtgrijs
+#'- inbo.grijsblauw
+#'- inbo.lichtblauw
+#'- inbo.achtergrond
+#'- inbo.munt
+#'- inbo.lichtgroen
+#'- inbo.donkergroen
+#'- inbo.groen
+#'- inbo.geelgr
 #'@docType data
 #'@export inbo.hoofd inbo.steun.donkerroos inbo.steun.blauw inbo.steun.geelgroen inbo.oranje inbo.rood inbo.bruinrood inbo.grijs inbo.lichtgrijs inbo.grijsblauw inbo.lichtblauw inbo.achtergrond inbo.munt inbo.lichtgroen inbo.donkergroen inbo.groen inbo.geelgr
-#'@usage inbo.hoofd
 #'@keywords datasets
 #'@seealso \code{\link{theme_inbo2015}}
 NULL
 
-#'@name INBOblue
+#'@name inbo_colours_2014
+#'@rdname inbo_colours_2014
 #'@aliases INBOblue INBObrown INBOdarkblue INBOdarkgreen INBOgreen INBOred INBOreddishbrown INBOextra
 #'@title Colour according to the INBO style guide version <= 2014
 #'@author Thierry Onkelinx, Nicole De Groof
-#'@description Colour according to the INBO style guide as hexadecimal values.
-#'\itemize{
-#'  \item INBOblue
-#'  \item INBOdarkblue
-#'  \item INBOgreen
-#'  \item INBOdarkgreen
-#'  \item INBObrown
-#'  \item INBOreddishbrown
-#'  \item INBOred
-#'  \item INBOextra
-#'}
+#'@family colours
+#'@description Colours according to the INBO style guide as hexadecimal values.
+#'- INBOblue
+#'- INBOdarkblue
+#'- INBOgreen
+#'- INBOdarkgreen
+#'- INBObrown
+#'- INBOreddishbrown
+#'- INBOred
+#'- INBOextra
 #'@docType data
 #'@export INBOblue INBObrown INBOdarkblue INBOdarkgreen INBOgreen INBOred INBOreddishbrown INBOextra
-#'@usage INBOblue
 #'@keywords datasets
 #'@seealso \code{\link{theme_INBO}}
 NULL
 
-#'@name page.height
+#'@name dimensions
+#'@rdname dimensions
 #'@aliases page.width column.width medium.width
+#'@family paper_size
 #'@title Standard dimensions for the Elsevier style guide
 #'@description Several standard dimensions for the Elsevier style guide
-#'\itemize{
-#'  \item{page.height}{Maximal height of a figuur (in inch)}
-#'  \item{page.width}{Width of a figure covering two columns (in inch)}
-#'  \item{column.width}{Width of a figure covering one column (in inch)}
-#'  \item{medium.width}{Width of a figure covering 1.5 columns (in inch)}
-#'}
+#'-`page.height` Maximal height of a figuur (in inch)
+#'-`page.width` Width of a figure covering two columns (in inch)
+#'-`column.width` Width of a figure covering one column (in inch)
+#'-`medium.width` Width of a figure covering 1.5 columns (in inch)
 #'@docType data
 #'@export page.height page.width column.width medium.width
-#'@usage page.height
 #'@keywords datasets
 #'@seealso \code{\link{theme_elsevier}}
 NULL
 
-#'@name vl.yellow
-#'@aliases vl.darkyellow vl.black vl.grey1 vl.grey2 vl.grey3 vl.grey4 vl.grey5 vl.grey6 vl.grey7 vl.lightgreen vl.darkgreen vl.lightblue vl.darkblue vl.lightred vl.darkred vl.lightbrown vl.darkbrown
+#'@rdname vlaanderen_colours
+#'@name vlaanderen_colours
+#'@family colours
+#'@aliases vl.yellow vl.darkyellow vl.black vl.grey1 vl.grey2 vl.grey3 vl.grey4 vl.grey5 vl.grey6 vl.grey7 vl.lightgreen vl.darkgreen vl.lightblue vl.darkblue vl.lightred vl.darkred vl.lightbrown vl.darkbrown
 #'@title Some colours for the corporate style of vlaanderen.be. Taken from level 1, and level 2 pallette 7
 #'@references http://webstijlgids.vlaanderen.be/element/kleurgebruik
 #'@author Thierry Onkelinx
 #'@description Colour according to the Vlaanderen style guide as hexadecimal values.
-#'\itemize{
-#'  \item vl.yellow
-#'  \item vl.darkyellow
-#'  \item vl.black
-#'  \item vl.grey1
-#'  \item vl.grey2
-#'  \item vl.grey3
-#'  \item vl.grey4
-#'  \item vl.grey5
-#'  \item vl.grey6
-#'  \item vl.grey7
-#'  \item vl.lightgreen
-#'  \item vl.darkgreen
-#'  \item vl.lightblue
-#'  \item vl.darkblue
-#'  \item vl.lightred
-#'  \item vl.darkred
-#'  \item vl.lightbrown
-#'  \item vl.darkbrown
-#'}
+#'- vl.yellow
+#'- vl.darkyellow
+#'- vl.black
+#'- vl.grey1
+#'- vl.grey2
+#'- vl.grey3
+#'- vl.grey4
+#'- vl.grey5
+#'- vl.grey6
+#'- vl.grey7
+#'- vl.lightgreen
+#'- vl.darkgreen
+#'- vl.lightblue
+#'- vl.darkblue
+#'- vl.lightred
+#'- vl.darkred
+#'- vl.lightbrown
+#'- vl.darkbrown
 #'@docType data
 #'@export vl.yellow vl.darkyellow vl.black vl.grey1 vl.grey2 vl.grey3 vl.grey4 vl.grey5 vl.grey6 vl.grey7 vl.lightgreen vl.darkgreen vl.lightblue vl.darkblue vl.lightred vl.darkred vl.lightbrown vl.darkbrown
-#'@usage vl.yellow
 #'@keywords datasets
 NULL
