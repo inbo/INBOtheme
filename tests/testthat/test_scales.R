a8f2f1f144c2f95c8be75d407b347054815b6412
ThierryO <thierry.onkelinx@inbo.be>
2018-11-26 10:09:05 +0100

    add more unit tests for the scales

--- tests/testthat/test_scales.R
+++ tests/testthat/test_scales.R
@@ -16,6 +16,39 @@ expect_identical(
   scale_fill_gradient()$palette(seq(0, 1, length = 2)),
   c(inbo.lichtblauw, inbo.rood)
 )
+expect_identical(
+  scale_colour_gradient2()$palette(seq(0, 1, length = 3)),
+  c(inbo.lichtblauw, "#FFFFFF", inbo.rood)
+)
+expect_identical(
+  scale_fill_gradient2()$palette(seq(0, 1, length = 3)),
+  c(inbo.lichtblauw, "#FFFFFF", inbo.rood)
+)
+ggplot2::theme_set(theme_vlaanderen2015())
+expect_identical(
+  scale_colour_discrete()$palette(9),
+  vlaanderen.2015.colours(9)
+)
+expect_identical(
+  scale_fill_discrete()$palette(9),
+  vlaanderen.2015.colours(9)
+)
+expect_identical(
+  scale_colour_gradient()$palette(seq(0, 1, length = 2)),
+  c(vl.lightblue, vl.lightred)
+)
+expect_identical(
+  scale_fill_gradient()$palette(seq(0, 1, length = 2)),
+  c(vl.lightblue, vl.lightred)
+)
+expect_identical(
+  scale_colour_gradient2()$palette(seq(0, 1, length = 3)),
+  c(vl.lightblue, "#FFFFFF", vl.lightred)
+)
+expect_identical(
+  scale_fill_gradient2()$palette(seq(0, 1, length = 3)),
+  c(vl.lightblue, "#FFFFFF", vl.lightred)
+)
 ggplot2::theme_set(ggplot2::theme_grey())
 expect_equal(
   scale_colour_discrete(),
@@ -32,5 +65,13 @@ expect_identical(
 expect_identical(
   scale_fill_gradient()$palette(seq(0, 1, length = 2)),
   c("#132B43", "#56B1F7")
+)
+expect_identical(
+  scale_colour_gradient2()$palette(seq(0, 1, length = 3)),
+  c("#132B43", "#FFFFFF", "#56B1F7")
+)
+expect_identical(
+  scale_fill_gradient2()$palette(seq(0, 1, length = 3)),
+  c("#132B43", "#FFFFFF", "#56B1F7")
 )
 ggplot2::theme_set(theme_inbo())
