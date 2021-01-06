ac67abe6f4461fc4de7ca197ba30194c0b483fd7
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-01-06 13:54:36 +0100

    add traffic_palette()

--- R/inbo_2015_colours.R
+++ R/inbo_2015_colours.R
@@ -52,3 +52,32 @@ Reduce the number of factor levels.",
   )
   palette[seq_len(n)]
 }
+
+#' A Colour Palette Ranging From a Dark Red over Medium Orange to Light Green.
+#'
+#' @export
+#' @inheritParams inbo_palette
+#' @return a vector of n hexadecimal values defining the colours.
+#' @family colours
+#' @importFrom assertthat assert_that is.count noNA
+#' @importFrom grDevices hcl
+traffic_palette <- function(n) {
+  if (missing(n)) {
+    n <- 3
+  }
+  assert_that(is.count(n), noNA(n), n >= 3)
+  dark_red <- c(12, 75, 20)
+  mid_orange <- c(31, 80, 50)
+  light_green <- c(127, 93, 80)
+  light_range <- seq(dark_red[3], light_green[3], length = n)
+  red_orange <- light_range <= mid_orange[3]
+  prop_red <- (mid_orange[3] - light_range[red_orange]) /
+    (mid_orange[3] - dark_red[3])
+  prop_green <- (light_range[!red_orange] - mid_orange[3]) /
+    (light_green[3] - mid_orange[3])
+  palette <- cbind(
+    outer(dark_red, prop_red) + outer(mid_orange, 1 - prop_red),
+    outer(light_green, prop_green) + outer(mid_orange, 1 - prop_green)
+  )
+  hcl(palette[1, ], palette[2, ], palette[3, ])
+}
