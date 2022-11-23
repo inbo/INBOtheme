c03af7338c8feeffce3286e9283230c5e6f6c101
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-11-23 15:34:12 +0100

    remove startup message

--- R/on_load.R
+++ R/on_load.R
@@ -5,25 +5,3 @@
   switch_colour(new_colour = inbo_steun_blauw)
   # nocov end
 }
-
-.onAttach <- function(libname, pkgname) { # nolint
-  packageStartupMessage(
-    "
-################################################################################
-#                                                                              #
-#          WARNING: important changes in INBOtheme version 0.5.7.              #
-#                                                                              #
-################################################################################
-
- * We removed items relating to the oldest style. These items contained
-   `INBO` in their name.
- * We renamed the individual colors to match the INBO coding style guide which
-   requires snake_case (underscore instead of dots for separation).
-   `inbo.hoofd` became `inbo_hoofd`.
-
- If you need an older version of INBOtheme, install it using the code below
-
- remotes::install_github(\"inbo/INBOtheme@v0.5.6\")
-"
-  )
-}
